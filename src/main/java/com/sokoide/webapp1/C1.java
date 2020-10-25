53d4d3c78095e44527dbea17529ac5a8d5584a85
So Koide <sokoide@gmail.com>
2020-10-25 23:07:48 +0900

    Added custom metrics

--- src/main/java/com/sokoide/webapp1/C1.java
+++ src/main/java/com/sokoide/webapp1/C1.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.microsoft.applicationinsights.TelemetryClient;
 
 import java.util.Random;
 
@@ -14,22 +15,33 @@ import static java.lang.String.format;
 @RequestMapping("/c1")
 @Scope("request")
 public class C1 {
-    Random rand = new Random();
+    private Random rand = new Random();
+    private TelemetryClient telemetry = new TelemetryClient();
+
+    public C1(){
+    }
 
     @GetMapping("/foo/{id}")
     public String fooAction(@PathVariable("id") String id){
+        telemetry.trackEvent("foo");
+        telemetry.trackPageView("fooPage");
         String ret = String.format("Hello %s", id);
         return ret;
     }
 
     @GetMapping("/hello")
     public String hello() {
+        telemetry.trackEvent("hello");
+        telemetry.trackPageView("helloPage");
         return "Hello";
     }
 
     @GetMapping("/random")
     public String random() {
+        telemetry.trackEvent("random");
+        telemetry.trackPageView("randomPage");
         int r = rand.nextInt(1000)+10;
+        telemetry.trackMetric("randomValue", r);
         try {
             Thread.sleep(r);
         } catch (InterruptedException e) {
