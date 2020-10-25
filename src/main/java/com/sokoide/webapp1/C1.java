b8d24a4a6903363c7ea711db80e157a60c1d0511
So Koide <sokoide@gmail.com>
2020-10-25 22:44:57 +0900

    Configured AppInsights

--- src/main/java/com/sokoide/webapp1/C1.java
+++ src/main/java/com/sokoide/webapp1/C1.java
@@ -6,16 +6,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 import static java.lang.String.format;
 
 @RestController
 @RequestMapping("/c1")
 @Scope("request")
 public class C1 {
+    Random rand = new Random();
+
     @GetMapping("/foo/{id}")
     public String fooAction(@PathVariable("id") String id){
         String ret = String.format("Hello %s", id);
         return ret;
     }
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+
+    @GetMapping("/random")
+    public String random() {
+        int r = rand.nextInt(1000)+10;
+        try {
+            Thread.sleep(r);
+        } catch (InterruptedException e) {
+        }
+        String ret = String.format("%s msec slept", r);
+        return ret;
+    }
 }
